7a6e492e55f626ae017afc6d5ac5ed84aafd6c6a
Firdous Ali <firdous.ali.tp@gmail.com>
2023-09-06 20:00:17 +0500

    lazy loading eg

--- src/containers/LocaleTest/index.js
+++ src/containers/LocaleTest/index.js
@@ -1,6 +1,15 @@
-import {useState} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import {useState, lazy, Suspense} from 'react';
+import {View, Text, TouchableOpacity, ActivityIndicator} from 'react-native';
 import {LocalizationHelper} from '../../helpers';
+// import {MyCustomControl} from '../../controls';
+
+// const ComplexComponent = lazy(() => import('../../containers/SignupScreen'));
+
+const Calendar = lazy(() => {
+  return new Promise(resolve => setTimeout(resolve, 5 * 1000)).then(() =>
+    import('../../containers/SignupScreen'),
+  );
+});
 
 const LocaleTest = () => {
   const [someVal, setSomeVal] = useState(undefined);
@@ -11,6 +20,10 @@ const LocaleTest = () => {
 
   return (
     <View>
+      <Suspense fallback={<ActivityIndicator />}>
+        <Calendar />
+      </Suspense>
+
       <Text>test locale</Text>
       <Text>{LocalizationHelper.t('hello')}</Text>
 
